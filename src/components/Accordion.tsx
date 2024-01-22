6e7b6e58a551a99ac2df198d70cd71af36f56f23
jjalbuenacabuyao <eustascaptainkid01@gmail.com>
2024-01-22 12:46:02 +0800

    initial implementation

---
+++ src/components/Accordion.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import {
+  Root,
+  AccordionSingleProps,
+  AccordionHeader,
+  AccordionItem,
+  AccordionContent,
+  AccordionTrigger,
+} from "@radix-ui/react-accordion";
+import React, { ReactNode } from "react";
+
+interface AccordionProps extends AccordionSingleProps {
+  variant: string;
+  dropDownIcon: ReactNode;
+  items: {
+    header: string;
+    content: string;
+  }[];
+};
+
+const Accordion = ({
+  className,
+  dropDownIcon,
+  items,
+  type="single",
+  variant,
+  ...props
+}: AccordionProps) => {
+  return (
+    <Root type={type} className={cn("", className)} {...props}>
+      {items.map(({ header, content }) => (
+        <AccordionItem value={header}>
+          <AccordionHeader>
+            <AccordionTrigger>
+              {header}
+              {dropDownIcon}
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent>{content}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Root>
+  );
+};
+
+export default Accordion;
