3ab05f89af45150446e92f657e1a44c232810b2e
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-01-23 22:25:34 +0800

    accordion story

---
+++ src/stories/Accordion.stories.ts
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Accordion from "../components/Accordion";
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta = {
+  title: "Accordion",
+  component: Accordion,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+} satisfies Meta<typeof Accordion>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    type: "single",
+    defaultValue: "1",
+    items: [
+      {
+        value: "1",
+        heading: "Hi World!",
+        content: "This is the content.",
+      },
+      {
+        value: "2",
+        heading: "Hi!",
+        content: "This is the content.",
+      },
+    ],
+  },
+};
