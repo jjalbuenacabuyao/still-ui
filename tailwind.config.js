806274f1a257faf45ef034fade010fa3b4e44423
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-01-25 19:44:07 +0800

    add animation and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        slideDown: "slideDown 150ms cubic-bezier(0.87, 0, 0.13, 1)",
+        slideUp: "slideUp 150ms cubic-bezier(0.87, 0, 0.13, 1)",
+      },
+      keyframes: {
+        slideDown: {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        slideUp: {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
