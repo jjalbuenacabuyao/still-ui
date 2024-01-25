929ba5920ba2fe0dad7c211221c8310a450f4058
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-01-25 19:43:56 +0800

    add placeholder texts

--- src/stories/Accordion.stories.ts
+++ src/stories/Accordion.stories.ts
@@ -16,14 +16,20 @@ export const Primary: Story = {
   args: {
     items: [
       {
-        value: "1",
-        heading: "Hi World!",
-        content: "This is the content.",
+        value: "item-1",
+        heading: "Lorem ipsum dolor sit amet?",
+        subtitle:
+          "Pharetra et ultrices neque ornare aenean euismod elementum nisi.",
+        content:
+          "Labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
       },
       {
-        value: "2",
-        heading: "Hi!",
-        content: "This is the content.",
+        value: "item-2",
+        heading:
+          "Consectetur adipiscing elit, sed do eiusmod tempor incididunt?",
+        subtitle: "Egestas dui id ornare arcu odio ut sem.",
+        content:
+          "Morbi tristique senectus et netus. Tristique senectus et netus et malesuada fames ac turpis egestas. Ullamcorper malesuada proin libero nunc consequat interdum varius sit.",
       },
     ],
   },
