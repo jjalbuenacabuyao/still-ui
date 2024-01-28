9304b259feffb97d57d85c407130ee4b13b779f6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-01-28 18:04:00 +0800

    move directory

---
+++ src/components/Accordion/src/hooks/AccordionContext.ts
@@ -0,0 +1,8 @@
+import { createContext } from "react";
+
+type AccordionOptions = {
+  variant?: "default";
+  bordered?: boolean;
+};
+
+export const AccordionContext = createContext<AccordionOptions>({});
