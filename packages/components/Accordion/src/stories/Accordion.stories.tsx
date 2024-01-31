333dd9f75e23e3c6e40c9146aea5895c74996992
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-01-31 19:57:10 +0800

    add story for accordion with subtitle

--- packages/components/Accordion/src/stories/Accordion.stories.tsx
+++ packages/components/Accordion/src/stories/Accordion.stories.tsx
@@ -53,6 +53,56 @@ const DefaultStyleTemplate = (args: AccordionRootProps) => (
   </AccordionRoot>
 );
 
+const AccordionWithSubtitleTemplate = (args: AccordionRootProps) => (
+  <AccordionRoot {...args}>
+    <AccordionItem value="item-1">
+      <AccordionTrigger
+        title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit?"}
+        subtitle={
+          "Aenean condimentum, urna ac pellentesque scelerisque, massa tellus rhoncus sapien, congue tristique odio ex mollis nunc."
+        }
+      />
+      <AccordionContent>
+        Nulla ornare, nulla at commodo condimentum, nibh elit cursus enim, a
+        imperdiet dolor urna ut felis. Curabitur finibus efficitur dolor, eu
+        blandit odio fermentum imperdiet. Cras tincidunt ut libero nec
+        fringilla. Aenean consequat egestas efficitur. Pellentesque ultricies
+        cursus velit, eu sodales sapien semper quis.
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-2">
+      <AccordionTrigger
+        title={"Pellentesque vel sapien nisl?"}
+        subtitle={
+          "Fusce placerat pellentesque urna quis condimentum. Vivamus vitae tellus molestie, ultricies justo ac, volutpat ipsum."
+        }
+      />
+      <AccordionContent>
+        Fusce finibus justo ut dui auctor fermentum. Nulla facilisi. Curabitur
+        quis enim vel felis accumsan faucibus. Maecenas mattis lacus sit amet
+        risus porttitor tristique. Duis mollis fringilla nunc, at vulputate odio
+        faucibus eget. Sed tortor metus, eleifend id tellus id, interdum gravida
+        purus. Integer at vehicula diam.
+      </AccordionContent>
+    </AccordionItem>
+    <AccordionItem value="item-3">
+      <AccordionTrigger
+        title={"Donec consectetur dolor at leo ultricies ultrices?"}
+        subtitle={
+          "In vehicula, dui ullamcorper dapibus varius, odio augue porta tellus, eget iaculis neque dui auctor mauris."
+        }
+      />
+      <AccordionContent>
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Mauris porta enim et eros vulputate, at dapibus leo
+        semper. Sed et justo pretium, porttitor urna eget, dictum est. Nulla
+        quis feugiat nisl. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit.
+      </AccordionContent>
+    </AccordionItem>
+  </AccordionRoot>
+);
+
 export const Default: Story = {
   render: DefaultStyleTemplate,
   args: {
@@ -76,3 +126,11 @@ export const Bordered: Story = {
     bordered: true,
   },
 };
+
+export const WithSubtitle: Story = {
+  render: AccordionWithSubtitleTemplate,
+  args: {
+    type: "single",
+    collapsible: true,
+  }
+}
