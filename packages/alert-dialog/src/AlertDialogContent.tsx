27688a89f630d41e332db0c0b057d7d21b5d600f
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-03-05 20:48:51 +0800

    initial implementation

---
+++ packages/alert-dialog/src/AlertDialogContent.tsx
@@ -0,0 +1,16 @@
+import { AlertDialogContentProps, Content } from "@radix-ui/react-alert-dialog"
+import { tv } from "tailwind-variants"
+
+const content = tv({
+  base: "fixed top-1/2 left-1/2 translate-x-1/2 translate-y-1/2"
+})
+
+const AlertDialogContent = ({children, className, ...props}: AlertDialogContentProps) => {
+  return (
+    <Content className={content({class: className})} {...props}>
+      {children}
+    </Content>
+  )
+}
+
+export default AlertDialogContent
