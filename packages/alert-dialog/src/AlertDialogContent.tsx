1d3a574b67e6663c0be1a9daf9336b7abe0755c6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-03-06 22:46:31 +0800

    add max-width

--- packages/alert-dialog/src/AlertDialogContent.tsx
+++ packages/alert-dialog/src/AlertDialogContent.tsx
@@ -2,7 +2,7 @@ import { AlertDialogContentProps, Content } from "@radix-ui/react-alert-dialog"
 import { tv } from "tailwind-variants"
 
 const content = tv({
-  base: "fixed top-1/2 left-1/2 translate-x-1/2 translate-y-1/2"
+  base: "fixed top-1/2 left-1/2 translate-x-1/2 translate-y-1/2 max-w-lg"
 })
 
 const AlertDialogContent = ({children, className, ...props}: AlertDialogContentProps) => {
