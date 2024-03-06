8baf23552b1ef2659e611e6cee66c9e6f05eaea6
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-03-06 22:50:40 +0800

    refactor type name

--- packages/alert-dialog/src/AlertDialogTrigger.tsx
+++ packages/alert-dialog/src/AlertDialogTrigger.tsx
@@ -1,11 +1,11 @@
 import { AlertDialogTriggerProps, Trigger } from '@radix-ui/react-alert-dialog';
 import { Button } from '@still-ui/button';
 
-type Props = {
+type TriggerProps = {
   buttonType?: "success" | "danger" | "outline" | "ghost";
 }
 
-const AlertDialogTrigger = ({buttonType, className, children, ...props}: Props & AlertDialogTriggerProps) => {
+const AlertDialogTrigger = ({buttonType, className, children, ...props}: TriggerProps & AlertDialogTriggerProps) => {
   return (
     <Trigger asChild>
       <Button variant={buttonType} className={className} {...props}>{children}</Button>
