f05f868ec26186754d6c5c52363aa65fdbcd4d9d
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-03-13 22:33:47 +0800

    export components

---
+++ packages/alert-dialog/src/index.ts
@@ -0,0 +1,21 @@
+import AlertDialogAction from "./AlertDialogAction";
+import AlertDialogCancel from "./AlertDialogCancel";
+import AlertDialogContent from "./AlertDialogContent";
+import AlertDialogDescription from "./AlertDialogDescription";
+import AlertDialogOverlay from "./AlertDialogOverlay";
+import AlertDialogPortal from "./AlertDialogPortal";
+import AlertDialogRoot from "./AlertDialogRoot";
+import AlertDialogTitle from "./AlertDialogTitle";
+import AlertDialogTrigger from "./AlertDialogTrigger";
+
+export {
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogOverlay,
+  AlertDialogPortal,
+  AlertDialogRoot,
+  AlertDialogTitle,
+  AlertDialogTrigger
+}
