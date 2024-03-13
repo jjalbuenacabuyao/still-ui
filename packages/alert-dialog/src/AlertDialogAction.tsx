85936a66cbaa817ce4591f0d05193a84fe07a069
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-03-13 22:36:17 +0800

    remove button component

--- packages/alert-dialog/src/AlertDialogAction.tsx
+++ packages/alert-dialog/src/AlertDialogAction.tsx
@@ -1,14 +1,9 @@
 import { Action } from "@radix-ui/react-alert-dialog";
-import { Button } from "@still-ui/button";
 import React, { ComponentPropsWithoutRef, ElementRef } from "react";
 
 const AlertDialogAction = React.forwardRef<
   ElementRef<typeof Action>,
   ComponentPropsWithoutRef<typeof Action>
->(({ children, ...props }, ref) => (
-  <Action asChild ref={ref}>
-    <Button {...props} />
-  </Action>
-));
+>(({ ...props }, ref) => <Action {...props} ref={ref} />);
 
 export default AlertDialogAction;
--- packages/alert-dialog/src/AlertDialogCancel.tsx
+++ packages/alert-dialog/src/AlertDialogCancel.tsx
@@ -1,15 +1,9 @@
 import { Cancel } from "@radix-ui/react-alert-dialog";
-import { Button } from "@still-ui/button";
 import React, { ComponentPropsWithoutRef, ElementRef } from "react";
-
 
 const AlertDialogCancel = React.forwardRef<
   ElementRef<typeof Cancel>,
   ComponentPropsWithoutRef<typeof Cancel>
->(({ ...props }, ref) => (
-  <Cancel asChild ref={ref}>
-    <Button {...props} />
-  </Cancel>
-));
+>(({ ...props }, ref) => <Cancel {...props} ref={ref} />);
 
 export default AlertDialogCancel;
