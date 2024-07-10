21fc835a4e582beca63717e1e5d3d3f57cb41790
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-10 19:11:29 +0800

    remove the context and use group classname in tailwind to adjust style based on data-splitted

--- packages/accordion/src/AccordionItem.tsx
+++ packages/accordion/src/AccordionItem.tsx
@@ -1,30 +1,20 @@
-import { useContext, ElementRef, ComponentPropsWithoutRef } from "react";
-import { AccordionContext } from "./hooks/AccordionContext";
+import { ElementRef, ComponentPropsWithoutRef } from "react";
 import { tv } from "tailwind-variants";
 import React from "react";
 import { Item } from "@radix-ui/react-accordion";
 
 const item = tv({
-  base: "group border-b border-b-slate-200 last:border-0",
-  variants: {
-    splitted: {
-      true: "rounded border border-slate-200 shadow-md last:border",
-    },
-  },
+  base: [
+    "group border-b border-b-slate-200 last:border-0",
+    "group-data-[splitted=true]/accordion:rounded group-data-[splitted=true]/accordion:border group-data-[splitted=true]/accordion:border-slate-200 group-data-[splitted=true]/accordion:shadow-md group-data-[splitted=true]/accordion:last:border",
+  ],
 });
 
 const AccordionItem = React.forwardRef<
   ElementRef<typeof Item>,
   ComponentPropsWithoutRef<typeof Item>
 >(({ className, ...props }, ref) => {
-  const { splitted } = useContext(AccordionContext);
-  return (
-    <Item
-      ref={ref}
-      className={item({ splitted, class: className })}
-      {...props}
-    />
-  );
+  return <Item ref={ref} className={item({ class: className })} {...props} />;
 });
 
 export default AccordionItem;
