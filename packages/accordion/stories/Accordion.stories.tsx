2393e32e51ce589854f62d0055faa95828e411e1
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-10 19:16:41 +0800

    add AccordionHeader component

--- packages/accordion/stories/Accordion.stories.tsx
+++ packages/accordion/stories/Accordion.stories.tsx
@@ -1,11 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import AccordionRoot, { AccordionRootProps } from "../src/AccordionRoot";
-import AccordionItem from "../src/AccordionItem";
-import AccordionTrigger from "../src/AccordionTrigger";
-import AccordionContent from "../src/AccordionContent";
+import { AccordionRootProps } from "../src/AccordionRoot";
 //@ts-ignore
 import React from "react";
+import {
+  AccordionHeader,
+  AccordionRoot,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "../src";
 
 const meta = {
   title: "Accordion",
@@ -19,9 +23,11 @@ type Story = StoryObj<typeof meta>;
 const DefaultStyleTemplate = (args: AccordionRootProps) => (
   <AccordionRoot {...args}>
     <AccordionItem value="item-1">
-      <AccordionTrigger
-        title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit?"}
-      />
+      <AccordionHeader>
+        <AccordionTrigger>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit?
+        </AccordionTrigger>
+      </AccordionHeader>
       <AccordionContent>
         Nulla ornare, nulla at commodo condimentum, nibh elit cursus enim, a
         imperdiet dolor urna ut felis. Curabitur finibus efficitur dolor, eu
@@ -30,55 +36,11 @@ const DefaultStyleTemplate = (args: AccordionRootProps) => (
         cursus velit, eu sodales sapien semper quis.
       </AccordionContent>
     </AccordionItem>
-    <AccordionItem value="item-2">
-      <AccordionTrigger title={"Pellentesque vel sapien nisl?"} />
-      <AccordionContent>
-        Fusce finibus justo ut dui auctor fermentum. Nulla facilisi. Curabitur
-        quis enim vel felis accumsan faucibus. Maecenas mattis lacus sit amet
-        risus porttitor tristique. Duis mollis fringilla nunc, at vulputate odio
-        faucibus eget. Sed tortor metus, eleifend id tellus id, interdum gravida
-        purus. Integer at vehicula diam.
-      </AccordionContent>
-    </AccordionItem>
-    <AccordionItem value="item-3">
-      <AccordionTrigger
-        title={"Donec consectetur dolor at leo ultricies ultrices?"}
-      />
-      <AccordionContent>
-        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
-        ridiculus mus. Mauris porta enim et eros vulputate, at dapibus leo
-        semper. Sed et justo pretium, porttitor urna eget, dictum est. Nulla
-        quis feugiat nisl. Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit.
-      </AccordionContent>
-    </AccordionItem>
-  </AccordionRoot>
-);
 
-const AccordionWithSubtitleTemplate = (args: AccordionRootProps) => (
-  <AccordionRoot {...args}>
-    <AccordionItem value="item-1">
-      <AccordionTrigger
-        title={"Lorem ipsum dolor sit amet, consectetur adipiscing elit?"}
-        subtitle={
-          "Aenean condimentum, urna ac pellentesque scelerisque, massa tellus rhoncus sapien, congue tristique odio ex mollis nunc."
-        }
-      />
-      <AccordionContent>
-        Nulla ornare, nulla at commodo condimentum, nibh elit cursus enim, a
-        imperdiet dolor urna ut felis. Curabitur finibus efficitur dolor, eu
-        blandit odio fermentum imperdiet. Cras tincidunt ut libero nec
-        fringilla. Aenean consequat egestas efficitur. Pellentesque ultricies
-        cursus velit, eu sodales sapien semper quis.
-      </AccordionContent>
-    </AccordionItem>
     <AccordionItem value="item-2">
-      <AccordionTrigger
-        title={"Pellentesque vel sapien nisl?"}
-        subtitle={
-          "Fusce placerat pellentesque urna quis condimentum. Vivamus vitae tellus molestie, ultricies justo ac, volutpat ipsum."
-        }
-      />
+      <AccordionHeader>
+        <AccordionTrigger>Pellentesque vel sapien nisl?</AccordionTrigger>
+      </AccordionHeader>
       <AccordionContent>
         Fusce finibus justo ut dui auctor fermentum. Nulla facilisi. Curabitur
         quis enim vel felis accumsan faucibus. Maecenas mattis lacus sit amet
@@ -88,12 +50,11 @@ const AccordionWithSubtitleTemplate = (args: AccordionRootProps) => (
       </AccordionContent>
     </AccordionItem>
     <AccordionItem value="item-3">
-      <AccordionTrigger
-        title={"Donec consectetur dolor at leo ultricies ultrices?"}
-        subtitle={
-          "In vehicula, dui ullamcorper dapibus varius, odio augue porta tellus, eget iaculis neque dui auctor mauris."
-        }
-      />
+      <AccordionHeader>
+        <AccordionTrigger>
+          Donec consectetur dolor at leo ultricies ultrices?
+        </AccordionTrigger>
+      </AccordionHeader>
       <AccordionContent>
         Orci varius natoque penatibus et magnis dis parturient montes, nascetur
         ridiculus mus. Mauris porta enim et eros vulputate, at dapibus leo
@@ -129,16 +90,8 @@ export const Bordered: Story = {
   },
 };
 
-export const WithSubtitle: Story = {
-  render: AccordionWithSubtitleTemplate,
-  args: {
-    type: "single",
-    collapsible: true,
-  },
-};
-
 export const Splitted: Story = {
-  render: AccordionWithSubtitleTemplate,
+  render: DefaultStyleTemplate,
   args: {
     type: "single",
     collapsible: true,
