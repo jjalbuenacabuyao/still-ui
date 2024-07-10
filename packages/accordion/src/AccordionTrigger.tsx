f12f0703b5062605a5fbec7a11a779720829a9d9
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-10 19:15:04 +0800

    remove the context and use the group to adjust style in splitted mode

--- packages/accordion/src/AccordionTrigger.tsx
+++ packages/accordion/src/AccordionTrigger.tsx
@@ -1,66 +1,39 @@
 import {
   AccordionTriggerProps as RadixAccordionTriggerProps,
   AccordionTrigger as Trigger,
-  AccordionHeader,
 } from "@radix-ui/react-accordion";
-import { useContext, ReactNode, ElementRef } from "react";
-import { AccordionContext } from "./hooks/AccordionContext";
+import { ReactNode, ElementRef } from "react";
 import { tv } from "tailwind-variants";
 import { ChevronDownIcon } from "@radix-ui/react-icons";
 import React from "react";
 
 type AccordionTriggerProps = {
-  title: string | ReactNode;
-  subtitle?: string | ReactNode;
   icon?: ReactNode;
-  classNames?: {
-    innerWrapper?: string;
-    title?: string;
-    subtitle?: string;
-    icon?: string;
-  };
 };
 
 const trigger = tv({
   slots: {
     wrapper:
-      "group flex w-full items-center justify-between gap-4 p-4 text-left",
-    innerWrapper: "flex flex-col gap-1",
-    tvTitle: "text-base",
-    tvSubtitle: "text-xs",
-    tvIcon: "transition-transform group-data-[state=open]:rotate-180",
-  },
-  variants: {
-    splitted: {
-      true: { wrapper: "p-6" },
-    },
-  },
+      "group/trigger flex w-full items-center justify-between gap-4 p-4 text-left group-data-[splitted=true]/accordion:p-6",
+    chevronIcon: "transition-transform group-data-[state=open]/trigger:rotate-180",
+  }
 });
 
-const { wrapper, innerWrapper, tvTitle, tvSubtitle, tvIcon } = trigger();
+const { wrapper, chevronIcon } = trigger();
 
 const AccordionTrigger = React.forwardRef<
   ElementRef<typeof Trigger>,
   RadixAccordionTriggerProps & AccordionTriggerProps
->(({ className, classNames, icon, subtitle, title, ...props }, ref) => {
-  const { splitted } = useContext(AccordionContext);
-
+>(({ className, children, icon, ...props }, ref) => {
   return (
-    <AccordionHeader>
       <Trigger
         ref={ref}
-        className={wrapper({ class: className, splitted })}
+        className={wrapper({ class: className })}
         {...props}
       >
-        <div className={innerWrapper({ class: classNames?.innerWrapper })}>
-          <span className={tvTitle({ class: classNames?.title })}>{title}</span>
-          <span className={tvSubtitle({ class: classNames?.subtitle })}>
-            {subtitle}
-          </span>
-        </div>
-          {icon ? icon : <ChevronDownIcon className={tvIcon({class: classNames?.icon})} />}
+        {children}
+        {icon ? icon : <ChevronDownIcon className={chevronIcon()} />}
       </Trigger>
-    </AccordionHeader>
   );
 });
 
