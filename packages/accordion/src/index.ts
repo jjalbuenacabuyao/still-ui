59e1ceecd3c19e4d74d2afc1351b4647644abdfc
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-10 19:15:31 +0800

    export AccordionHeader component

--- packages/accordion/src/index.ts
+++ packages/accordion/src/index.ts
@@ -2,10 +2,12 @@ import AccordionRoot from "./AccordionRoot";
 import AccordionItem from "./AccordionItem";
 import AccordionTrigger from "./AccordionTrigger";
 import AccordionContent from "./AccordionContent";
+import AccordionHeader from "./AccordionHeader";
 
 export {
   AccordionRoot,
   AccordionItem,
   AccordionTrigger,
   AccordionContent,
+  AccordionHeader,
 }
