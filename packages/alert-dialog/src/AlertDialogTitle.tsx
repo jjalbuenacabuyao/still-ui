92bdc926d3351cc6695fe23f24f4b3b5f0abe979
Joel Cabuyao <eustascaptainkid01@gmail.com>
2024-07-26 21:03:47 +0800

    remove title prop and replace it with the children prop

--- packages/alert-dialog/src/AlertDialogTitle.tsx
+++ packages/alert-dialog/src/AlertDialogTitle.tsx
@@ -3,20 +3,16 @@ import React from "react";
 import { ElementRef } from "react";
 import { tv } from "tailwind-variants";
 
-type TitleProps = {
-  title: string | React.ReactNode;
-};
-
 const titleClass = tv({
   base: "text-lg font-semibold",
 });
 
 const AlertDialogTitle = React.forwardRef<
   ElementRef<typeof Title>,
-  AlertDialogTitleProps & TitleProps
->(({ className, title, ...props }, ref) => (
+  AlertDialogTitleProps
+>(({ className, children, ...props }, ref) => (
   <Title className={titleClass({ class: className })} {...props} ref={ref}>
-    {title}
+    {children}
   </Title>
 ));
 
